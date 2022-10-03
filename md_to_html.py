220879872003ba020659f014c08979bee0f4faf0
Replit user <>
2022-10-03 12:53:42 +0000

    Improved the md_to_html file with user input and error checks

--- md_to_html.py
+++ md_to_html.py
@@ -1,8 +1,18 @@
 import markdown
+import os
+import sys
 
-with open("README.md", "r") as file:
+print("--- Markdown to HTML ---")
+fname = input("Enter  a file name: ")
+
+if not os.path.exists(fname):
+    print("Sorry, we couldn't find that file")
+    sys.exit(1)
+
+with open(fname, "r") as file:
     text = file.read()
     html = markdown.markdown(text)
 
-with open("README.html", "w") as file:
+html_fname = input("Where should we save the html? ")
+with open(html_fname, "w") as file:
     file.write(html)
